e1240de4fabebf562b7e9ea185e04fcae2169f4a
Om-Midya <dukemidya7521@gmail.com>
2024-07-14 23:53:00 +0530

    forgot to save add auth and middleware for auth

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,4 +1,3 @@
-// src/middlewares/authMiddleware.js
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,4 +5,52 @@ const authController = require("../controllers/authController");
 
 // Add your auth routes here
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *       400:
+ *         description: Username already exists
+ */
+router.post("/register", authController.register);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *       401:
+ *         description: Invalid username or password
+ */
+router.post("/login", authController.login);
+
 module.exports = router;

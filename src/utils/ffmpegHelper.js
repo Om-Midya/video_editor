bde1a9b7fd484f15933089b847ab1d22c7e18988
Om-Midya <dukemidya7521@gmail.com>
2024-07-15 03:29:29 +0530

    Fixed ffmpeg helper error

--- src/utils/ffmpegHelper.js
+++ src/utils/ffmpegHelper.js
@@ -1,49 +1,35 @@
+// src/utils/ffmpegHelper.js
 const ffmpeg = require("fluent-ffmpeg");
+const ffprobeStatic = require("ffprobe-static");
 
-const getVideoInfo = (inputPath) => {
+ffmpeg.setFfprobePath(ffprobeStatic.path);
+
+const getVideoInfo = (filePath) => {
   return new Promise((resolve, reject) => {
-    ffmpeg.ffprobe(inputPath, (err, metadata) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(metadata);
-      }
+    ffmpeg.ffprobe(filePath, (err, metadata) => {
+      if (err) return reject(err);
+      resolve(metadata);
     });
   });
 };
 
-const trimVideo = (inputPath, outputPath, startTime, duration) => {
+const trimVideo = (inputPath, outputPath, startTime, endTime) => {
   return new Promise((resolve, reject) => {
     ffmpeg(inputPath)
       .setStartTime(startTime)
-      .setDuration(duration)
+      .setDuration(endTime - startTime)
       .output(outputPath)
-      .on("end", () => {
-        resolve();
-      })
-      .on("error", (err) => {
-        reject(err);
-      })
+      .on("end", resolve)
+      .on("error", reject)
       .run();
   });
 };
 
 const mergeVideos = (inputPaths, outputPath) => {
   return new Promise((resolve, reject) => {
-    const ffmpegCommand = ffmpeg();
-
-    inputPaths.forEach((inputPath) => {
-      ffmpegCommand.input(inputPath);
-    });
-
-    ffmpegCommand
-      .on("end", () => {
-        resolve();
-      })
-      .on("error", (err) => {
-        reject(err);
-      })
-      .mergeToFile(outputPath);
+    const command = ffmpeg();
+    inputPaths.forEach((inputPath) => command.input(inputPath));
+    command.on("end", resolve).on("error", reject).mergeToFile(outputPath);
   });
 };
 
